f77e298ca3287743f51abafdcabe64ce1dea5db4
neodevelop <juan@makingdevs.com>
2017-03-30 10:00:30 -0600

    Primer ejercicio de closures

---
+++ grupo_alpha/dia2/closures.groovy
@@ -0,0 +1,12 @@
+
+def discriminante(a,b,c){
+  (b*b) - (4*a*c)
+}
+
+def unaSolucion(a,b,c){
+  def d = discriminante(a,b,c)
+  if(d < 0) throw new RuntimeException("No tiene solución")
+  ((-1*b) - Math.sqrt(d)) / (2*a) 
+}
+
+println "Solución para 1x^2 + 16x + 64 es = ${unaSolucion(1,16,64)}"
