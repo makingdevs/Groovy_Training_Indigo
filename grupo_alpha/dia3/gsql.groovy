07026b0f5a218a1816af29d14f4bdac4021db06a
neodevelop <juan@makingdevs.com>
2017-04-04 13:26:00 -0500

    GSQL bases de datos con Groovy

---
+++ grupo_alpha/dia3/gsql.groovy
@@ -0,0 +1,56 @@
+@GrabConfig(systemClassLoader=true)
+@Grab(group='mysql', module='mysql-connector-java', version='5.1.6')
+import groovy.sql.Sql
+
+db = [
+  username: "makingdevs",
+  password: "makingdevs",
+  url: "jdbc:mysql://localhost/issue_tracker",
+  driver: "com.mysql.jdbc.Driver"
+]
+
+sql = Sql.newInstance(db.url, db.username, db.password, db.driver)
+
+sql.execute """
+create table if not exists issue(
+id integer primary key auto_increment,
+description varchar(255),
+priority integer,
+date_created datetime);
+"""
+
+sql.execute """
+	insert into issue(description, priority, date_created) values
+	('No jala',4, now()),
+	('No esta bonito', 3, now()),
+	('En mi local si jala', 1, now())
+"""
+
+issue1 = ["No se muestran los clientes",1, new Date()-30]
+
+sqlInsert = "insert into issue(description, priority, date_created) values(?,?,?)"
+sql.execute sqlInsert, issue1
+
+//sql.eachRow("select * from issue") { row ->
+//	println row
+//}
+
+dataset = sql.dataSet('issue')
+
+filterIssues = dataset.findAll { row ->
+	row.id >= 5 && row.id <=9
+}
+println filterIssues.sql
+filterIssues.each { row ->
+	println "${row.description} - ${row[3]}"
+}
+
+
+dataset.add(
+	description: "Ultimo issue",
+	priority: 10,
+  date_created: new Date() - 20
+)
+
+counter = sql.firstRow("select count(*) as counter from issue limit 0,1;")['counter']
+println "Hay ${counter} registros"
