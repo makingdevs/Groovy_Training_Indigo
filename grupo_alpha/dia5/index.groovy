a58cb46f78aa9720bf002cf6d11e775b462bfeea
neodevelop <juan@makingdevs.com>
2017-04-11 16:04:54 -0500

    Uso de Groovlets

---
+++ grupo_alpha/dia5/index.groovy
@@ -0,0 +1,13 @@
+//rango = 1..100
+//println "Hola inmundo ${new Date()}"
+//rango.each {
+//  println "$it<br/>"
+//}
+users = Followers.instance.followersOf("neodevelop")
+
+//response.contentType = 'application/json'
+//json(users)
+println session
+println headers
+println request
+println params
