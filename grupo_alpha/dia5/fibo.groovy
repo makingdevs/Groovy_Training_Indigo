cbbeeb213bf095becccbb59957bb05b85c62ea8b
neodevelop <juan@makingdevs.com>
2017-04-11 15:58:13 -0500

    Agregando tipo al closure

--- grupo_alpha/dia5/fibo.groovy
+++ grupo_alpha/dia5/fibo.groovy
@@ -1,12 +1,12 @@
 def fiboMem;
-def fibo = { it < 2 ? it : fiboMem(it-1) + fiboMem(it-2) }
+def fibo = { n -> n< 2 ? n : fiboMem(n-1) + fiboMem(n-2) }
 fiboMem = fibo.memoize()
 
-def resultado = 0;
-def iterator = 1;
-def elementos = [];
+def resultado = 0
+def iterator = 1
+def elementos = []
 while(resultado < 4000000) {
-    resultado = fiboMem(iterator++);
+    resultado = fiboMem(iterator++)
     resultado % 2 == 0 && elementos << resultado
 }
 
