27a917a0e3eef667dbf21bbee13264e2c4399e08
neodevelop <juan@makingdevs.com>
2017-04-20 17:02:28 -0500

    Se me olvido un archivo

--- grupo_beta/Otro.java
+++
@@ -1,7 +0,0 @@
-public class Otro {
-  public static void main(String... args){
-    HolaMundo2 h = new HolaMundo2();
-    h.setNombre("Juan");
-    System.out.println(h.saluda());
-  }
-}
--- grupo_beta/saluda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-s = "touch file.txt".execute().text
-
-println s
-
-for(int i = 0; i< 100; i++){
-  System.out.println(i);
-}
