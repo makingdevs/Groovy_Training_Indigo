b359a0dd88be77e06e6d62659c39433e769bb0ae
neodevelop <juan@makingdevs.com>
2017-04-25 15:25:19 -0500

    Agregando Swing con Groovy

---
+++ grupo_beta/dia3/swing.groovy
@@ -0,0 +1,7 @@
+import javax.swing.*
+import java.awt.*
+import java.awt.event.*
+
+frame = new JFrame(layout: new FlowLayout(), size:[300, 300], title:"Hola Indigo")
+println frame.class
+frame.show()
