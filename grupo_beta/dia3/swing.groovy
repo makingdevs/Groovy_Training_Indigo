f2e582325fa10ee35e86c5bc75e3d2aeb80bf1dd
neodevelop <juan@makingdevs.com>
2017-04-25 15:43:26 -0500

    Implementando una interfaz con diversos métodos

--- grupo_beta/dia3/swing.groovy
+++ grupo_beta/dia3/swing.groovy
@@ -13,6 +13,7 @@ frame.contentPane.add button
 frame.contentPane.add label1
 frame.contentPane.add positionLabel
 frame.contentPane.add msgLabel
+frame.contentPane.add new JTextField()
 
 button.addActionListener({ println "Hola mundo" } as ActionListener)
 
@@ -21,6 +22,11 @@ displayMouseLocation = { positionLabel.text = "${it.x}, ${it.y}" }
 frame.addMouseMotionListener(displayMouseLocation as MouseMotionListener)
 frame.addMouseListener(displayMouseLocation as MouseListener)
 
+handleFocus = [
+  focusGained: { msgLabel.text = "You're in..." },
+  focusLost: { msgLabel.text = "You're OUT!!! " }
+]
 
+button.addFocusListener(handleFocus as FocusListener)
 
 frame.show()
