460bf864d4d05b5f451846937f43ee4d7184cca5
neodevelop <juan@makingdevs.com>
2017-05-04 11:13:51 -0500

    Quitando arreglog , refactor

--- grupo_alpha/dia6/StringCalculator.groovy
+++ grupo_alpha/dia6/StringCalculator.groovy
@@ -6,7 +6,7 @@ int add( String values ) {
    else {
     def separator = ","
     if( values.contains(separator) ) {
-      def arr = values.split(separator).collect {Integer.parseInt( it )}.sum()    
+      values.split(separator).collect {Integer.parseInt( it )}.sum()    
     } else {
       Integer.parseInt( values )
     }
