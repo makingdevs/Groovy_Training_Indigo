409619de6a32b3968d9fb84421b4d12f3b6d1879
neodevelop <juan@makingdevs.com>
2017-05-04 23:19:40 -0500

    Agregando una prueba de caché usando Stub

--- grupo_alpha/dia6/CalculatorTest.groovy
+++ grupo_alpha/dia6/CalculatorTest.groovy
@@ -5,7 +5,6 @@ class CalculatorTest extends GroovyTestCase {
   void testSumOperation(){
     
     def memoryStub = new StubFor( Memory )
-
     memoryStub.demand.findOperation { String op, values -> null }
     memoryStub.demand.saveOperation { String op, result, values -> }
 
@@ -13,7 +12,24 @@ class CalculatorTest extends GroovyTestCase {
       Calculator c = new Calculator()
       assert c.operation("+", 2, 3) == 5
     }
-    
+    memoryStub.expect.verify()
+  }
+
+  void testSumOperationWithCache(){
+    def memoryStub = new StubFor( Memory )
+    def cache = [:]
+    memoryStub.demand.findOperation(4) { String op, values ->
+      cache?."$op $values"
+    }
+    memoryStub.demand.saveOperation(2) { String op, result, values -> 
+      cache."$op $values" = result
+    }
+    memoryStub.use {
+      Calculator c = new Calculator()
+      assert c.operation("+", 2, 3) == 5
+      assert c.operation("+", 5, 5) == 10
+      assert c.operation("+", 2, 3) == 5
+    }
     memoryStub.expect.verify()
   }
 }
