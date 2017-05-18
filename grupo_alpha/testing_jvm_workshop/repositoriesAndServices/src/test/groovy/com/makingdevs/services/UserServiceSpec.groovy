4cb0f5888fd8f508672da7b217e9fa03050580d2
neodevelop <juan@makingdevs.com>
2017-05-18 11:31:49 -0500

    Vamos a usar datos de BD para realizar el set de pruebas

--- grupo_alpha/testing_jvm_workshop/repositoriesAndServices/src/test/groovy/com/makingdevs/services/UserServiceSpec.groovy
+++ grupo_alpha/testing_jvm_workshop/repositoriesAndServices/src/test/groovy/com/makingdevs/services/UserServiceSpec.groovy
@@ -6,9 +6,12 @@ import com.makingdevs.model.Project
 import com.makingdevs.model.UserStory
 import com.makingdevs.repositories.ProjectRepository
 import com.makingdevs.repositories.UserStoryRepository
+import groovy.sql.Sql
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by neodevelop on 18/05/17.
@@ -25,13 +28,23 @@ class UserServiceSpec extends Specification {
   @Autowired
   UserStoryRepository userStoryRepository
 
+  @Shared sql = Sql.newInstance(
+      'jdbc:mysql://localhost/db_test',
+      'makingdevs',
+      'makingdevs',
+      'com.mysql.jdbc.Driver')
+
+  @Unroll
   def "Add a new user story to an existent project"(){
     given:"An existent project"
-      Project p = projectRepository.findOne(1L)
+      Project p = projectRepository.findOne(id)
       def userStoriesInitial = userStoryRepository.findAllByProject(p)
 
     and: "a new user story"
-      UserStory us = new UserStory(description: "uniq desc", priority: 3, effort: 4)
+      UserStory us = new UserStory(
+          description: description,
+          priority: priority,
+          effort: effort)
 
     when:"we try to add the US to this project"
       userStoryService.createUserStory(us, p)
@@ -39,9 +52,15 @@ class UserServiceSpec extends Specification {
 
     then:"the project should have one more user story"
       userStoriesFinal.size() == userStoriesInitial.size() + 1
-      userStoriesFinal.any { u -> u.description == "uniq desc" }
+      userStoriesFinal.any { u -> u.description == description }
       us.project == p
       userStoriesFinal.every { u -> u.project.id == p.id }
+    where:
+    [id,description, priority,effort] << sql.rows("select * from data_test")
+    //id << [1L,2L,3L]
+    //description << ["Desc1","Desc2","Desc3"]
+    //priority << [5,4,1]
+    //effort << [7,3,1]
   }
 
   def "Add a new user story to a new project"(){}
